2e0e07bf37939b972d8ccad7556f41800b295475
Malte Lucé <malte.luce@appmotion.de>
2024-02-12 09:28:05 +0100

    improv(configScreen): Improve of rowPicker in ui aspect

--- app/src/main/java/com/example/todomanager/ui/ticketconfig/configelements/picker/RowPicker.kt
+++ app/src/main/java/com/example/todomanager/ui/ticketconfig/configelements/picker/RowPicker.kt
@@ -1,12 +1,15 @@
 package com.example.todomanager.ui.ticketconfig.configelements.picker
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Create
@@ -16,7 +19,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.example.todomanager.data.ticket.Ticket
 
@@ -28,24 +33,45 @@ fun RowPicker(
 ) {
     Column(
         modifier = modifier
-            .padding(horizontal = 2.dp)
             .background(
-                color = MaterialTheme.colorScheme.onBackground,
+                color = MaterialTheme.colorScheme.surface,
                 shape = RoundedCornerShape(16.dp),
             )
             .padding(8.dp),
     ) {
-        Text(text = "Status des Tickets")
+        Text(
+            text = "Status des Tickets",
+            color = MaterialTheme.colorScheme.onSurface,
+            style = MaterialTheme.typography.titleMedium,
+        )
         Spacer(modifier = Modifier.height(8.dp))
         Row(
             modifier = Modifier
+                .border(
+                    BorderStroke(
+                        width = Dp.Hairline,
+                        color = MaterialTheme.colorScheme.onSurface,
+                    ),
+                    shape = RoundedCornerShape(16.dp),
+                )
+                .clip(RoundedCornerShape(16.dp))
                 .clickable(onClick = onShowRowPickerDialog)
                 .padding(4.dp),
             verticalAlignment = Alignment.CenterVertically,
         ) {
-            Text(text = row.toString())
+            Spacer(Modifier.width(12.dp))
+            Text(
+                text = row.toString().lowercase().replaceFirstChar { it.uppercase() },
+                color = MaterialTheme.colorScheme.onSurface,
+                style = MaterialTheme.typography.labelLarge,
+            )
             Spacer(Modifier.weight(1f))
-            Icon(imageVector = Icons.Default.Create, contentDescription = null)
+            Icon(
+                imageVector = Icons.Default.Create,
+                contentDescription = null,
+                tint = MaterialTheme.colorScheme.onSurface,
+            )
+            Spacer(Modifier.width(8.dp))
         }
     }
 }
